9b16f10a82c2af0dcb04509ef570d47e7ce8c24b
Chih-Yuan Peng <slycp1@exmail.nottingham.ac.uk>
2022-05-27 20:21:33 +0100

    fix: length operator

--- server/gen_workload.py
+++ server/gen_workload.py
@@ -2,7 +2,7 @@ import numpy as np
 import csv
 import random
 
-def gen_workload(min_w:int, max_w:int, len:int, job_scheduling:bool, real_workload:bool):
+def gen_workload(min_w:int, max_w:int, length:int, job_scheduling:bool, real_workload:bool):
     # {'192.168.200.78': 5000, '192.168.82.190': 11000, '192.168.193.97': 11000, '192.168.65.162': 11000, '192.168.112.40': 11000, '192.168.171.114': 5000, '192.168.98.17': 5000, '192.168.110.43': 5000, '192.168.9.14': 5000}
     if real_workload:
         task_cpu = []
@@ -18,7 +18,7 @@ def gen_workload(min_w:int, max_w:int, len:int, job_scheduling:bool, real_worklo
                         task_cpu.append(cpu*1000)
                         cpu_sum += cpu
         workload = []
-        r = [random.random() for i in range(1,len+1)]
+        r = [random.random() for i in range(1,length+1)]
         s = sum(r)
         r = [ i/s for i in r ]
 
@@ -31,9 +31,9 @@ def gen_workload(min_w:int, max_w:int, len:int, job_scheduling:bool, real_worklo
     else:
         if job_scheduling:
             # 1 task need 100 
-            workload = np.random.randint(1,10, size=[len,1])*100
+            workload = np.random.randint(1,10, size=[length,1])*100
         else:
-            workload = np.random.randint(min_w,max_w+1, size=[len,1])
+            workload = np.random.randint(min_w,max_w+1, size=[length,1])
         total_task = 0
         for w in np.transpose(workload)[0]:
             total_task += w / 100
